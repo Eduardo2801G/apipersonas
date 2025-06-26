a78767b5e9cf500c34bcc843d92ed6ab88695686
Eduardo <sornozadavid8@gmail.com>
2025-06-26 13:05:24 -0600

    Fix CORS for local frontend

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const personasRouter = require('./routes/personas');
 require('dotenv').config();
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/personas', personasRouter);
